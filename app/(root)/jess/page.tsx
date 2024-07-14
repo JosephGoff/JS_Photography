f76ee999377d2397226a63beb33c4bf9fe42f9cf
JosephGoff <joeygoff13@gmail.com>
2024-07-13 23:38:41 -0400

    7/13 11pm

--- app/(root)/jess/page.tsx
+++ app/(root)/jess/page.tsx
@@ -1,9 +1,20 @@
-import Navbar from '@/components/Navbar/Navbar'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
 import appData from "../../../app-data.json";
 
 const Jess = () => {
+  const mdImageRef = useRef<HTMLImageElement>(null);
+  const smImageRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (mdImageRef.current && smImageRef.current) {
+      mdImageRef.current.style.opacity = "1";
+      smImageRef.current.style.opacity = "1";
+    }
+  }, []);
+
   return (
     <div>
       <Navbar />
@@ -12,40 +23,91 @@ const Jess = () => {
           width: "100vw",
           height: "calc(100vh - 60px)",
           display: "flex",
-          overflow: 'hidden',
+          overflow: "hidden",
           zIndex: 101,
-          position: 'relative',
-          // backgroundColor: "#E4E2DD"
-          backgroundColor: "#D7CDBE"
+          position: "relative",
+          backgroundColor: "#D7CDBE",
         }}
       >
+        <div
+          className="hidden md:flex"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "50%",
+            transform: "translateX(-50%)",
+            minHeight: "calc(100vh - 60px)",
+            aspectRatio: "2.889 / 2",
+            zIndex: 102,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* <div
+            className="hidden md:block"
+            style={{ width: "100px", height: "100px", backgroundColor: "blue" }}
+          ></div> */}
+        </div>
+
         <Image
+          ref={mdImageRef}
           className="hidden md:block"
           src={appData.baseURL + "jess/jess1.png"}
           alt="header"
           style={{
-            position: 'absolute', 
+            opacity: 0,
+            position: "absolute",
             top: 0,
-            left: '50%',
-            transform: 'translateX(-50%)',
-            minHeight: "calc(100vh - 60px)"
+            left: "50%",
+            transform: "translateX(-50%)",
+            minHeight: "calc(100vh - 60px)",
+            transition: "1.5s ease",
+            zIndex: 101,
           }}
-          objectFit="contain" 
+          objectFit="contain"
           objectPosition="center"
           layout="fill"
         />
+
+        <div
+          className="flex md:hidden"
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%) translateY(-50%)",
+            minHeight: "calc(100vh - 60px)",
+            minWidth: "100vw",
+            aspectRatio: "1439 / 1999",
+            zIndex: 102,
+            justifyContent: "center",
+            alignItems: "center",
+            top: "50%",
+          }}
+        >
+          {/* <div
+            className="block md:hidden"
+            style={{ width: "100px", height: "100px", backgroundColor: "blue" }}
+          ></div> */}
+        </div>
+
         <Image
+          ref={smImageRef}
           className="block md:hidden"
-          src={appData.baseURL + "jess/jess.png"}
+          src={appData.baseURL + "jess/jess2.png"}
           alt="header"
-          style={{ minHeight: "calc(100vh - 60px) ", maxHeight: "calc(100vh - 60px) "}}
+          style={{
+            opacity: 0,
+            minHeight: "calc(100vh - 60px)",
+            maxHeight: "calc(100vh - 60px)",
+            transition: "1.5s ease",
+          }}
           layout="fill"
           objectFit="cover"
           objectPosition="center"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jess
+export default Jess;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -22,23 +22,22 @@ const Navbar = () => {
   const [currentRoute, setCurrentRoute] = useState("/");
   const router = usePathname()
 
-  useEffect(() => {
-    setCurrentRoute(router)
-  }, [router])
+  // useEffect(() => {
+  //   setCurrentRoute(router)
+  // }, [router])
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 760) {
-        closeIsOpen(false)
-      }
-    };
+  // useEffect(() => {
+  //   const handleNavResize = () => {
+  //     if (window.innerWidth >= 760) {
+  //       closeIsOpen(false)
+  //     }
+  //   };
 
-    window.addEventListener('resize', handleResize); // Fix typo here
-    handleResize();
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   window.addEventListener('resize', handleNavResize); // Fix typo here
+  //   return () => {
+  //     window.removeEventListener('resize', handleNavResize);
+  //   };
+  // }, []);
 
   function closeIsOpen(newVal: boolean) {
     if (newVal) {
